f021ca13fdb22a264d68c33544a04baf304f7faa
Zhenru <charlooottepeng@gmail.com>
2021-09-12 13:24:24 -0400

    first commit

---
+++ src/searchImg/models.py
@@ -0,0 +1,15 @@
+from django.db.models import ImageField, CharField, Model, TextField, DecimalField
+from django.db.models.fields import DecimalField
+from django_mysql.models import ListCharField
+
+# Create your models here.
+class img(Model):
+	image_upload  = ImageField(default = 'image', upload_to ='uploads/')
+	title = CharField(max_length=100)
+	characteristics = ListCharField(
+		base_field=CharField(max_length=10),
+        size=6,
+        max_length=(6 * 11)
+	)
+	description = TextField(null=True, blank=True)
+	price = DecimalField(decimal_places=2, max_digits=1000)
